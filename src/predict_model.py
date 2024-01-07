dac9c11daf89201816dc0b354b8f0536801ba2ba
JonasGRG <jonas@gjerris.dk>
2024-01-07 21:58:40 +0100

    implemented predit model

--- src/predict_model.py
+++ src/predict_model.py
@@ -1,17 +1,73 @@
+import click
 import torch
+from torch.utils.data import datasets, DataLoader
+from torchvision import transforms
 
-def predict(
-    model: torch.nn.Module,
-    dataloader: torch.utils.data.DataLoader
-) -> None:
-    """Run prediction for a given model and dataloader.
-    
-    Args:
-        model: model to use for prediction
-        dataloader: dataloader with batches
-    
-    Returns
-        Tensor of shape [N, d] where N is the number of samples and d is the output dimension of the model
+from src.models.model import ImageClassifier
+from src.utils.utils import create_class_mappings, load_yaml_config
 
-    """
-    return torch.cat([model(batch) for batch in dataloader], 0)
+
+# Example usage
+train_dir = 'data/raw/train'
+
+class_to_idx, idx_to_class = create_class_mappings(train_dir)
+
+@click.group()
+def cli():
+    """Command line interface."""
+    pass
+
+data_config = load_yaml_config("data/data_config.yaml")
+normalization_mean = data_config['mean']
+normalization_std = data_config['std']
+
+@click.command()
+@click.argument("model_checkpoint")
+@click.option("--data", default='data/raw/test/', help="Folder containing the data. Data can be .jpg, .pngs, .np, .pt")
+@click.option("--resize-dim", required=True, type=int, help="Resize dimension for the images")
+def predict(model_checkpoint: str, data: str, resize_dim: int):
+    model = ImageClassifier.load_from_checkpoint(
+        checkpoint_path=model_checkpoint,
+        map_location='cpu',
+    )
+    model.eval()
+
+    # Define the transforms
+    base_transforms = transforms.Compose([
+        transforms.Resize((resize_dim, resize_dim)),
+        transforms.ToTensor(),
+        transforms.Normalize(mean=normalization_mean, std=normalization_std),
+    ])
+
+    # Create the dataset and DataLoader
+    dataset = datasets.ImageFolder(root=data, transform=base_transforms)
+    inference_loader = DataLoader(dataset, batch_size=1, shuffle=False)
+
+    _, idx_to_class = create_class_mappings('data/raw/train')
+
+    results = {}
+    with torch.no_grad():
+        for images, indices in inference_loader:
+            logits = model(images)
+            probabilities = F.softmax(logits, dim=1)
+            top_prob, top_idx = probabilities.topk(1, dim=1)
+
+            # Get the image file name
+            image_path = dataset.samples[indices.item()][0]
+            image_file_name = os.path.basename(image_path)
+
+            # Store the results in a dictionary
+            results[image_file_name] = {
+                "certainty": top_prob.item(),
+                "class_name": idx_to_class[top_idx.item()]
+            }
+
+    return results
+
+cli.add_command(predict)
+
+if __name__ == "__main__":
+    cli()
+
+
+
