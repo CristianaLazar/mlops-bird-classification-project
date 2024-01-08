44653d1b6d92eaedf83676ecd7ff0c6b8a4b4f1f
JonasGRG <jonas@gjerris.dk>
2024-01-08 10:40:30 +0100

    updated predict to use hydra

--- src/predict_model.py
+++ src/predict_model.py
@@ -1,47 +1,38 @@
-import click
-from hydra.utils import to_absolute_path
+import hydra
+from omegaconf import DictConfig
 import torch
 from torch.utils.data import DataLoader
-from torchvision import datasets ,transforms
+from torchvision import datasets, transforms
 
 from src.models.model import ImageClassifier
-from src.utils.utils import create_class_mappings, load_yaml_config
-
-def setup_predict_model():
-    train_dir = to_absolute_path('data/raw/train')
-    return create_class_mappings(train_dir)
-
-class_to_idx, idx_to_class = None, None
+from src.utils.utils import create_class_mappings
 
 
-@click.group()
-def cli():
-    """Command line interface."""
-    pass
+@hydra.main(config_path="configs", config_name="default_config.yaml", version_base="1.1")
+def predict(config: DictConfig) -> None:
 
-@click.command()
-@click.argument("model_checkpoint")
-@click.option("--data", default='data/raw/test/', help="Folder containing the data. Data can be .jpg, .pngs, .np, .pt")
-@click.option("--resize-dim", required=True, type=int, help="Resize dimension for the images")
-def predict(model_checkpoint: str, data: str, resize_dim: int):
+    config = config.predict
+
+    train_dir = hydra.utils.to_absolute_path(config.data)
+    class_to_idx, idx_to_class = create_class_mappings(train_dir)
+
     model = ImageClassifier.load_from_checkpoint(
-        checkpoint_path=model_checkpoint,
+        checkpoint_path=config.model_checkpoint,
         map_location='cpu',
     )
     model.eval()
 
     # Define the transforms
     base_transforms = transforms.Compose([
-        transforms.Resize((resize_dim, resize_dim)),
+        transforms.Resize((config.resize_dim, config.resize_dim)),
         transforms.ToTensor(),
-        transforms.Normalize(mean=normalization_mean, std=normalization_std),
+        # Assuming normalization mean and std are part of your config
+        transforms.Normalize(mean=config.normalization.mean, std=config.normalization.std),
     ])
 
     # Create the dataset and DataLoader
-    dataset = datasets.ImageFolder(root=data, transform=base_transforms)
+    dataset = datasets.ImageFolder(root=config.data, transform=base_transforms)
     inference_loader = DataLoader(dataset, batch_size=1, shuffle=False)
-
-    _, idx_to_class = create_class_mappings('data/raw/train')
 
     results = {}
     with torch.no_grad():
@@ -62,14 +53,10 @@ def predict(model_checkpoint: str, data: str, resize_dim: int):
 
     return results
 
-cli.add_command(predict)
 
 if __name__ == "__main__":
-    class_to_idx, idx_to_class = setup_predict_model()
-    data_config = load_yaml_config("data/data_config.yaml")
-    normalization_mean = data_config['mean']
-    normalization_std = data_config['std']
-    cli()
+    predict()
+
 
 
 
